e7b6b12e5569fc50dac3f7424389afb7224abf6a
NewBee <31762504+TassGab@users.noreply.github.com>
2017-09-22 13:37:28 +0200

    Add files via upload
    Example to describe the class and its use

---
+++ Examples/Example_class/Example_scheduler_class.ino
@@ -0,0 +1,36 @@
+/*
+ * 10/09/2017
+ * Gabriele Tasselli
+ * 
+ * Scheduler for Chorno Thermostat
+ * weekly scheduler to be used with TimeAlarm library
+ * The schedule is stored in the eeprom
+ * For memory optimization the resolution is 15 minutes.
+ * Intially 4 events per day are allocated
+ */
+#include "Heater_scheduler_class.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  HeaterSchedulerCs HS=HeaterSchedulerCs(); //class instance
+  Serial.begin(9600);
+  Serial.print("Day Size=");
+  Serial.println(sizeof(HS.Sched.WeekSched[0]),DEC);
+  Serial.println(HS.SetEventDay(dowSunday,Event1,3,swOFF,sw2));
+   //time_t now_t=now();
+   time_t now_t;
+   now_t=HS.SetTimeOfDay(3,10,10);
+   Serial.println(HS.TimeToStr(now_t));
+   Serial.print("now in quarter=");
+   Serial.println(TimeToQuart(now_t));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+/*
+ * Functions
+ */
+ 
---
+++ Examples/Example_class/Heater_scheduler_class.h
@@ -0,0 +1,54 @@
+#ifndef HeaterSched_h
+#define HeaterSched_h
+#include "TimeLib.h"
+#include <Arduino.h>
+#define nEventPerDay 6 
+#define nEventFuture 2
+//Macro
+#define QuartToTime(_quarter_) (_quarter_ * SECS_PER_MIN * 15UL)
+#define TimeToQuart(_time_) (((_time_ % SECS_PER_DAY) / SECS_PER_MIN)/15UL)
+typedef uint8_t TimeQuarter_sc;
+enum EventState_sc:bool {evEN=true, evDIS=false} ; 
+enum SwitchState_sc:bool {swON=true, swOFF=false} ; 
+enum SwitchNum_sc:uint8_t {sw1=1, sw2=2} ;
+typedef enum {Event0, Event1, Event2, Event3, Event4, Event5} EventNum_sc;
+typedef struct
+{
+  EventState_sc isEnabled ; //Enable=1 disable=0
+  SwitchNum_sc nSwitch; //Switch 1=0 Switch 2=1
+  SwitchState_sc swTurn; //State=0 --> turn off, State=1 --> turn ON
+} EventCtrl_sc;
+typedef struct 
+{  
+  TimeQuarter_sc EvTimeQ; //Time of the event in quarter of hour
+  EventCtrl_sc EventCtrl; //Event control (ON OFF, En Dis, switch to turn
+}EventType_sc;
+typedef struct
+{
+  timeDayOfWeek_t dow; // day of the week
+  EventType_sc Event[nEventPerDay]; //array of events per day
+} DaySchedule_sc;
+typedef struct
+{
+  time_t TimeEv;
+  EventCtrl_sc EventCtrl;
+} EventFuture_sc;
+
+typedef struct
+{
+  DaySchedule_sc WeekSched[7]; //Array of DaySchedule_sc representing the week schedule
+  EventFuture_sc EventFuture[nEventFuture];
+} Sched_sc;
+class HeaterSchedulerCs
+{
+  public:
+    Sched_sc Sched;
+    String SetEventDay(timeDayOfWeek_t, EventNum_sc, TimeQuarter_sc, SwitchState_sc, SwitchNum_sc);
+    String SetEventOnce(EventNum_sc, time_t, SwitchState_sc, SwitchNum_sc);
+    String TimeToStr(time_t);
+    time_t SetTimeOfDay(uint8_t, uint8_t, uint8_t);
+  private:
+    String EventToStrShort(EventType_sc);
+    String EventToStrLong(EventType_sc);    
+};
+#endif
