3674d0af5133758ba34e640b82d0a8de5aa4cdfd
NewBee <31762504+TassGab@users.noreply.github.com>
2017-09-26 13:48:41 +0200

    Add files via upload

---
+++ Examples/Example_class_alarms/Example_scheduler_class_alarms.ino
@@ -0,0 +1,74 @@
+/*
+ * 10/09/2017
+ * Gabriele Tasselli
+ * 
+ * Scheduler for Chorno Thermostat
+ * weekly scheduler to be used with TimeAlarm library
+ * The schedule is stored in the eeprom
+ * For memory optimization the resolution is 15 minutes.
+ * Intially 4 events per day are allocated
+ */
+#include "Heater_scheduler_class.h"
+#include <TimeAlarms.h>
+AlarmId id;
+HeaterSchedulerCs HS=HeaterSchedulerCs(); //class instance
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+//  Serial.print("Day Size=");
+//  Serial.println(sizeof(HS.Sched.WeekSched[0]),DEC);
+  Serial.println(HS.SetEventDay(dowTuesday,Event0,0,swON,sw1));
+setTime(23,59,50,25,9,17); // set time
+   //time_t now_t=now();
+//   time_t now_t;
+//   now_t=HS.SetTimeOfDay(3,10,10);
+//   Serial.println(HS.TimeToStr(now_t));
+//   Serial.print("now in quarter=");
+//   Serial.println(TimeToQuart(now_t));
+   Alarm.alarmRepeat(0,0,0, UpdateSched);  // update schedule at midnight
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalClockDisplay();
+  Alarm.delay(1000); // wait one second between clock display
+}
+
+/*
+ * Functions
+ */
+ void OnceOnly() {
+  Serial.println("This timer only triggers once");
+  // use Alarm.free() to disable a timer and recycle its memory.
+  Alarm.free(id);
+  // optional, but safest to "forget" the ID after memory recycled
+  id = dtINVALID_ALARM_ID;
+  // you can also use Alarm.disable() to turn the timer off, but keep
+  // it in memory, to turn back on later with Alarm.enable().
+}
+void UpdateSched()
+{
+  Serial.println("update Schedule");
+  int dow=weekday();
+  if HS.Sched.WeekSched[dow].Event[Event0].EventCtrl.isEnabled==evEN
+  {
+  id=Alarm.timerOnce(HS.QuartToTime(HS.Sched.WeekSched[dow].Event[Event0].EvTimeQ), OnceOnly);
+  }
+}
+void digitalClockDisplay() {
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.println();
+}
+
+void printDigits(int digits) {
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
---
+++ Examples/Example_class_alarms/Heater_Scheduler_class.cpp
@@ -0,0 +1,97 @@
+#include "Heater_scheduler_class.h"
+#include <Arduino.h>
+String HeaterSchedulerCs::SetEventDay(timeDayOfWeek_t dow, EventNum_sc Evnum, TimeQuarter_sc TimeQ, SwitchState_sc SwSt, SwitchNum_sc SwN)
+ {
+   String s="Dow: ";   
+   Sched.WeekSched[dow-1].dow=dow;
+   Sched.WeekSched[dow-1].Event[Evnum].EvTimeQ=TimeQ;
+   Sched.WeekSched[dow-1].Event[Evnum].EventCtrl.isEnabled=evEN;
+   Sched.WeekSched[dow-1].Event[Evnum].EventCtrl.swTurn=SwSt;
+   Sched. WeekSched[dow-1].Event[Evnum].EventCtrl.nSwitch=SwN;
+//   Serial.print("DOW=");
+//   Serial.print(dayStr(dow));
+//   Serial.print("\t");
+//   Serial.print("Event=");
+//   Serial.print(Evnum);
+//   Serial.println(EventToStr(WeekSched[dow-1].Event[Evnum]));
+   s+=dayStr(dow);
+   s+=" ; EventNum: ";
+   s+=Evnum;
+   s+=" ; ";
+   s+=EventToStrShort(Sched.WeekSched[dow-1].Event[Evnum]);
+   return s;
+ }
+time_t HeaterSchedulerCs::QuartToTime(TimeQuarter_sc qt)
+{
+  time_t tt;
+  if (qt==0)
+  {
+    tt=1; //one second  after midnight
+  }
+  else
+  {
+    tt=qt*SECS_PER_MIN * 15;
+  }
+  return tt;
+}
+String HeaterSchedulerCs::EventToStrShort(EventType_sc Ev)
+{
+  String s="[";
+  s+=Ev.EvTimeQ;
+  s+=",";
+  s+=Ev.EventCtrl.isEnabled;
+  s+=",";
+  s+=Ev.EventCtrl.swTurn;
+  s+=",";
+  s+=Ev.EventCtrl.nSwitch;
+  s+="]";
+  return s;
+}
+String HeaterSchedulerCs::EventToStrLong(EventType_sc Ev)
+ {
+  String s="[Quarter: ";
+  s+=Ev.EvTimeQ;
+  s+=" (time: ";
+  //Serial.print("\nEvent=[time: ");
+  s+=TimeToStr(QuartToTime(Ev.EvTimeQ));
+  //Serial.print(" ; En: ");
+  s+=") ; En:"; 
+  //Serial.print(Ev.EventCtrl.isEnabled);
+  s+=Ev.EventCtrl.isEnabled ? "Enabled":"Disabled";
+  //Serial.print(" ; SwState: ");
+  s+=" ; SwState: ";
+  //Serial.print(Ev.EventCtrl.swTurn);
+  s+=Ev.EventCtrl.swTurn ? "ON":"OFF";
+  //Serial.print(" ; SwNum; ");
+  s+=" ; SwNum; ";
+  //Serial.print(Ev.EventCtrl.nSwitch);
+  s+=Ev.EventCtrl.nSwitch;
+  //Serial.println("]");
+  s+="]";
+  return s;
+ }
+String HeaterSchedulerCs::TimeToStr(time_t tt)
+ {
+  String s="";
+  s+=hour(tt);
+  s+=":";
+  s+=minute(tt);
+  s+=":";
+  s+=second(tt);
+//  Serial.print("\n");
+//  Serial.print(hour(tt));
+//  Serial.print(":");
+//  Serial.print(minute(tt));
+//  Serial.print(":");
+//  Serial.println(second(tt));
+  return s;
+ }
+ time_t HeaterSchedulerCs::SetTimeOfDay(uint8_t hh, uint8_t mm, uint8_t ss)
+ {
+  tmElements_t tt;
+  tt.Hour=hh;
+  tt.Minute=mm;
+  tt.Second=ss;
+  return makeTime(tt);
+ }
+
